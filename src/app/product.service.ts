ddc08c4c679aef8434ebde6cc4b1337b1aea9126
edgar chris <edgarchris4@gmail.com>
2019-10-24 21:04:05 +0300

    redefine method in arrow notation

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,8 +9,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum( id: Number )  {
+  getAlbum = (id: Number) => {
     return this._http.get(this._albumUrl).map(response => response.json());
   }
-
 }
