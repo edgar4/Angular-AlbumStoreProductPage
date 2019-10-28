ed49f226a619a08707f466bad79dea11217e344c
edgar chris <edgarchris4@gmail.com>
2019-10-28 18:03:56 +0300

    add a get products method

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -15,4 +15,5 @@ export class ProductService {
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
+   getProducts() { }
 }
