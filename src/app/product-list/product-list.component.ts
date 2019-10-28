4f3f153013b858925845b4983b370c133d070256
edgar chris <edgarchris4@gmail.com>
2019-10-28 18:21:20 +0300

    fix abunch of typos i made

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,10 +10,10 @@ import {ProductService} from '../product.service';
 export class ProductListComponent implements OnInit {
   products: Product[];
 
-  constructor(private _productServic: ProductService) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productServic.getProducts().subscribe( response => this.products = response)
+    this._productService.getProducts().subscribe( response => this.products = response)
   }
 
 }
